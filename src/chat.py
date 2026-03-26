5582abe1b5b812bacd02ecad13b162a5eaa48dc5
andre souza <andssouzza@gmail.com>
2026-03-25 21:04:56 -0400

    feat: deve executar o chat para trazer os dados solicitado pelo usuario

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,21 @@
 from search import search_prompt
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
-    chain = search_prompt()
+    question_in_context_1 = "Qual o faturamento da Empresa SuperTechIABrazil?"
+    question_without_context_1 = "Qual a localização da empresa SuperTechIABrazil?"
 
-    if not chain:
-        print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
-        return
-    
-    pass
+    print("Question One:\n")
+    print(question_in_context_1)
+    print(search_prompt(question_in_context_1))
+    print("="*50)
+    print("\nQuestion Two:\n")
+    print(question_without_context_1)
+    print(search_prompt(question_without_context_1))
+    print("="*50)
 
 if __name__ == "__main__":
     main()
